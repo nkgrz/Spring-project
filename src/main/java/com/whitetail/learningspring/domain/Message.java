6bb0fdd80f2c160ae3f149d193d08b31ed54e1ee
nkgrz <is911n@icloud.com>
2024-08-15 17:28:58 +0300

    Added a field for storing the image name

--- src/main/java/com/whitetail/learningspring/domain/Message.java
+++ src/main/java/com/whitetail/learningspring/domain/Message.java
@@ -12,6 +12,8 @@ public class Message {
     private String text;
     private String tag;
 
+    private String filename;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     private User author;
