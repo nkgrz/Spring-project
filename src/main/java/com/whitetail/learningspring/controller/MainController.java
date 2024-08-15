ead9b5a060a59bb1e978a014a801c090652661a9
nkgrz <is911n@icloud.com>
2024-08-15 17:28:09 +0300

    Accesses to the repository have been transferred to the service. Code optimization

--- src/main/java/com/whitetail/learningspring/controller/MainController.java
+++ src/main/java/com/whitetail/learningspring/controller/MainController.java
@@ -2,7 +2,7 @@ package com.whitetail.learningspring.controller;
 
 import com.whitetail.learningspring.domain.Message;
 import com.whitetail.learningspring.domain.User;
-import com.whitetail.learningspring.repository.MessageRepository;
+import com.whitetail.learningspring.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -10,17 +10,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 public class MainController {
 
-    private final MessageRepository messageRepository;
+    private final MessageService messageService;
 
     @Autowired
-    public MainController(MessageRepository messageRepository) {
-        this.messageRepository = messageRepository;
+    public MainController(MessageService messageService) {
+        this.messageService = messageService;
     }
 
     @GetMapping("/")
@@ -32,9 +34,9 @@ public class MainController {
     public String mainMap(@RequestParam(required = false, defaultValue = "") String tag, Model model) {
         List<Message> messages;
         if (tag != null && !tag.isEmpty()) {
-            messages = messageRepository.findByTag(tag);
+            messages = messageService.findByTag(tag);
         } else {
-            messages = messageRepository.findAll();
+            messages = messageService.findAll();
         }
         model.addAttribute("messages", messages);
         model.addAttribute("tag", tag);
@@ -45,30 +47,23 @@ public class MainController {
     public String add(
             @AuthenticationPrincipal User user,
             @RequestParam String text,
-            @RequestParam String tag, Model model
-    ) {
-        messageRepository.save(new Message(text, tag, user));
-        model.addAttribute("messages", messageRepository.findAll());
+            @RequestParam String tag,
+            @RequestParam("file") MultipartFile file,
+            Model model
+    ) throws IOException {
+        Message message = new Message(text, tag, user);
+        if (file != null && !file.isEmpty()) {
+            String fileName = messageService.saveImage(file);
+            message.setFilename(fileName);
+        }
+        messageService.save(message);
+        model.addAttribute("messages", messageService.findAll());
         return "main";
     }
 
     @PostMapping("delete")
     public String delete() {
-        messageRepository.deleteAll();
+        messageService.deleteMessages();
         return "redirect:/main";
     }
 }
-
-//@RestController
-//public class GreetingController {
-//
-//    private static final String template = "Hello, %s!";
-//    private final AtomicLong counter = new AtomicLong();
-//
-//    @GetMapping("/greeting")
-//    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-//        return new Greeting(counter.incrementAndGet(), String.format(template, name));
-//    }
-//}
-
-
