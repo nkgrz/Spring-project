9280b27199d60f1237e152dd81044a0e06abc06e
nkgrz <is911n@icloud.com>
2024-08-19 15:48:03 +0300

    The business logic has been brought to the service

--- src/main/java/com/whitetail/learningspring/controller/RegistrationController.java
+++ src/main/java/com/whitetail/learningspring/controller/RegistrationController.java
@@ -1,25 +1,23 @@
 package com.whitetail.learningspring.controller;
 
-import com.whitetail.learningspring.domain.Role;
 import com.whitetail.learningspring.domain.User;
-import com.whitetail.learningspring.repository.UserRepository;
+import com.whitetail.learningspring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.Collections;
 
 @Controller
 public class RegistrationController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
     @Autowired
-    public RegistrationController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/registration")
@@ -29,16 +27,27 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String registration(User user, Model model, RedirectAttributes redirectAttributes) {
-        User userExists = userRepository.findByUsername(user.getUsername());
-        if (userExists != null) {
-            model.addAttribute("message", "Username is already taken!");
+
+        if (!userService.addUser(user)) {
+            model.addAttribute("message", "User exists!");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
+
         redirectAttributes.addFlashAttribute("message", "Registration successful!");
         return "redirect:/login";
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(@PathVariable("code") String code, Model model) {
+        boolean isActivated = userService.activateUser(code);
+
+        if (isActivated) {
+            model.addAttribute("message", "User successfully activated!");
+        } else {
+            model.addAttribute("message", "Activation code is not found!");
+        }
+
+        return "login";
     }
 
 }
