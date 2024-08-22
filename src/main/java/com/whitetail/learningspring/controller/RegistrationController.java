ffb8acf5a8c8169cb8d15e494decbb6219a38af8
nkgrz <is911n@icloud.com>
2024-08-22 16:58:44 +0300

    Added validation of the entered data during user registration. Added an error message when trying to log in with incorrect data.

--- src/main/java/com/whitetail/learningspring/controller/RegistrationController.java
+++ src/main/java/com/whitetail/learningspring/controller/RegistrationController.java
@@ -2,13 +2,19 @@ package com.whitetail.learningspring.controller;
 
 import com.whitetail.learningspring.domain.User;
 import com.whitetail.learningspring.service.UserService;
+import com.whitetail.learningspring.validation.AllValidationGroups;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
 
 @Controller
 public class RegistrationController {
@@ -26,10 +32,28 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String registration(User user, Model model, RedirectAttributes redirectAttributes) {
+    public String registration(@Validated(AllValidationGroups.class) User user,
+                               BindingResult bindingResult,
+                               Model model,
+                               RedirectAttributes redirectAttributes) {
 
-        if (!userService.addUser(user)) {
-            model.addAttribute("errorMessage", "User exists!");
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllersUtils.getErrorsMap(bindingResult);
+            model.mergeAttributes(errors);
+            return "registration";
+        }
+        try {
+            userService.addUser(user);
+        } catch (Exception e) {
+            if (e.getMessage().contains("usernameError")) {
+                model.addAttribute("usernameError", "Username is already taken");
+            }
+            if (e.getMessage().contains("emailError")) {
+                model.addAttribute("emailError", "Email is already taken");
+            }
+            if (e.getMessage().contains("passwordError")) {
+                model.addAttribute("password2Error", "Password are different!");
+            }
             return "registration";
         }
 
@@ -44,9 +68,17 @@ public class RegistrationController {
         if (isConfirmed) {
             model.addAttribute("message", "The email has been successfully confirmed!");
         } else {
-            model.addAttribute("message", "Activation code is not found!");
+            model.addAttribute("errorMessage", "Activation code is not found!");
         }
 
+        return "login";
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error, Model model) {
+        if (error != null) {
+            model.addAttribute("errorMessage", "Invalid username or password.");
+        }
         return "login";
     }
 
