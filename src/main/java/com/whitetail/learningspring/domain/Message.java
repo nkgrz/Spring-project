9dd45eca72d8d1168aee5f38c910ac4b73cdc29a
nkgrz <is911n@icloud.com>
2024-08-22 17:53:59 +0300

    Added validations for the text field and tag

--- src/main/java/com/whitetail/learningspring/domain/Message.java
+++ src/main/java/com/whitetail/learningspring/domain/Message.java
@@ -1,7 +1,9 @@
 package com.whitetail.learningspring.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
@@ -9,7 +11,10 @@ public class Message {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Please fill the message")
+    @Length(max = 2048, message = "Message too long(more then 2048 symbols)")
     private String text;
+    @Length(max = 128, message = "Tag too long(more then 128 symbols)")
     private String tag;
 
     private String filename;
