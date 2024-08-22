36c3c5a4ce5050d2c8dc5f67465a8185b6b4b976
nkgrz <is911n@icloud.com>
2024-08-22 16:09:20 +0300

    Added checks for a new message, code optimization

--- src/main/java/com/whitetail/learningspring/controller/MainController.java
+++ src/main/java/com/whitetail/learningspring/controller/MainController.java
@@ -3,10 +3,12 @@ package com.whitetail.learningspring.controller;
 import com.whitetail.learningspring.domain.Message;
 import com.whitetail.learningspring.domain.User;
 import com.whitetail.learningspring.service.MessageService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainController {
@@ -46,17 +49,25 @@ public class MainController {
     @PostMapping("/main")
     public String add(
             @AuthenticationPrincipal User user,
-            @RequestParam String text,
-            @RequestParam String tag,
-            @RequestParam("file") MultipartFile file,
-            Model model
+            @Valid Message message,
+            BindingResult bindingResult,
+            Model model,
+            @RequestParam("file") MultipartFile file
     ) throws IOException {
-        Message message = new Message(text, tag, user);
-        if (file != null && !file.isEmpty()) {
-            String fileName = messageService.saveImage(file);
-            message.setFilename(fileName);
+        message.setAuthor(user);
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errorsMap = ControllersUtils.getErrorsMap(bindingResult);
+            model.mergeAttributes(errorsMap);
+            model.addAttribute("message", message);
+        } else {
+            if (file != null && !file.isEmpty()) {
+                String fileName = messageService.saveImage(file);
+                message.setFilename(fileName);
+            }
+            messageService.save(message);
+            model.addAttribute("message", null);
         }
-        messageService.save(message);
+
         model.addAttribute("messages", messageService.findAll());
         return "main";
     }
