d50bf0d6857b1998c9c2ad61f93c948b212dba90
nkgrz <is911n@icloud.com>
2024-08-22 16:06:50 +0300

    Added a method for getting errors in the map

---
+++ src/main/java/com/whitetail/learningspring/controller/ControllersUtils.java
@@ -0,0 +1,18 @@
+package com.whitetail.learningspring.controller;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class ControllersUtils {
+    static Map<String, String> getErrorsMap(BindingResult bindingResult) {
+        Collector<FieldError, ?, Map<String, String>> map = Collectors.toMap(
+                fieldError -> fieldError.getField() + "Error",
+                FieldError::getDefaultMessage);
+        return bindingResult.getFieldErrors().stream().collect(
+                map);
+    }
+}
