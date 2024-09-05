309da0404e2e06294c9a0b1f345b97d6038e4e87
nkgrz <is911n@icloud.com>
2024-09-05 16:21:40 +0300

    Pagination added

--- src/main/java/com/whitetail/learningspring/repository/MessageRepository.java
+++ src/main/java/com/whitetail/learningspring/repository/MessageRepository.java
@@ -1,12 +1,15 @@
 package com.whitetail.learningspring.repository;
 
 import com.whitetail.learningspring.domain.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    List<Message> findByTag(String tag);
+    Page<Message> findByTag(String tag, Pageable pageable);
     void deleteByAuthor_Id(Long userId);
     List<Message> findByAuthor_Id(Long userId);
+    Page<Message> findByAuthor_Id(Long userId, Pageable pageable);
 }
