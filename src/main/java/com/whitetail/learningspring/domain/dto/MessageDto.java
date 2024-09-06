bbc50139fc4528741c2a28b71719dc3d9cb6141c
nkgrz <is911n@icloud.com>
2024-09-07 00:01:37 +0300

    Added DTO for Message

---
+++ src/main/java/com/whitetail/learningspring/domain/dto/MessageDto.java
@@ -0,0 +1,41 @@
+package com.whitetail.learningspring.domain.dto;
+
+import com.whitetail.learningspring.domain.Message;
+import com.whitetail.learningspring.domain.User;
+import com.whitetail.learningspring.domain.util.MessageHelper;
+import lombok.Getter;
+
+@Getter
+public class MessageDto {
+    private final Long id;
+    private final String text;
+    private final String tag;
+    private final User author;
+    private final String filename;
+    private final Long likes;
+    private final Boolean meLiked;
+
+    public MessageDto(Message message, Long likes, Boolean meLiked) {
+        this.id = message.getId();
+        this.text = message.getText();
+        this.tag = message.getTag();
+        this.author = message.getAuthor();
+        this.filename = message.getFilename();
+        this.likes = likes;
+        this.meLiked = meLiked;
+    }
+
+    public String getAuthorName() {
+        return MessageHelper.getAuthorName(author);
+    }
+
+    @Override
+    public String toString() {
+        return "MessageDto{" +
+                "id=" + id +
+                ", author=" + author +
+                ", likes=" + likes +
+                ", meLiked=" + meLiked +
+                '}';
+    }
+}
