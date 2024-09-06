1922eab0bd9b902a63a0340b9e485ac6485c4f16
nkgrz <is911n@icloud.com>
2024-09-06 23:27:09 +0300

    Add repository methods for filtering messages by tag, user, and retrieving all messages with like status

--- src/main/java/com/whitetail/learningspring/repository/MessageRepository.java
+++ src/main/java/com/whitetail/learningspring/repository/MessageRepository.java
@@ -1,16 +1,51 @@
 package com.whitetail.learningspring.repository;
 
 import com.whitetail.learningspring.domain.Message;
+import com.whitetail.learningspring.domain.User;
+import com.whitetail.learningspring.domain.dto.MessageDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
-    Page<Message> findByTag(String tag, Pageable pageable);
+
     void deleteByAuthor_Id(Long userId);
+
     List<Message> findByAuthor_Id(Long userId);
-    Page<Message> findByAuthor_Id(Long userId, Pageable pageable);
+
     void deleteMessageById(Long userId);
+
+    @Query("select new com.whitetail.learningspring.domain.dto.MessageDto(" +
+            "        m, " +
+            "        count(ml), " +
+            "        (sum(case when ml = :user then 1 else 0 end)  > 0) " +
+            "        ) " +
+            "        from Message m left join m.likes ml " +
+            "        group by m ")
+    Page<MessageDto> findAll(Pageable pageable, @Param("user") User user);
+
+
+    @Query("select new com.whitetail.learningspring.domain.dto.MessageDto(" +
+            "        m, " +
+            "        count(ml), " +
+            "        (sum(case when ml = :user then 1 else 0 end)  > 0) " +
+            "        ) " +
+            "        from Message m left join m.likes ml " +
+            "        where m.tag = :tag " +
+            "        group by m ")
+    Page<MessageDto> findByTag(Pageable pageable, @Param("tag") String tag, @Param("user") User user);
+
+    @Query("select new com.whitetail.learningspring.domain.dto.MessageDto(" +
+            "        m, " +
+            "        count(ml), " +
+            "        (sum(case when ml = :currentUser then 1 else 0 end)  > 0) " +
+            "        ) " +
+            "        from Message m left join m.likes ml " +
+            "        where m.author = :user " +
+            "        group by m ")
+    Page<MessageDto> findByUser(Pageable pageable, @Param("user") User user, @Param("currentUser") User currentUser);
 }
